ab822cdedf04d713241b6601ce24fa7a5bc06ce4
Stefan Biereigel <stefan@biereigel.de>
2014-04-07 19:01:49 +0200

    convert unsigned to signed in matched filter

--- matched_filt/matched_filt.vhd
+++ matched_filt/matched_filt.vhd
@@ -24,12 +24,12 @@ entity matched_filt is
 end matched_filt;
 
 architecture behav of matched_filt is
-	type delay_sr is array (0 to filter_len-2) of signed(width-1 downto 0);
+	type delay_sr is array (0 to filter_len-2) of unsigned(width-1 downto 0);
 	signal delay : delay_sr;
 	constant sum_len : integer := integer(ceil(log2(real(filter_len))));
 begin
 	process
-		variable sum : signed(width+sum_len-1 downto 0);
+		variable sum : unsigned(width+sum_len-1 downto 0);
 	begin
 		wait until rising_edge(clk);
 		outclk <= '0';
@@ -39,12 +39,12 @@ begin
 		else 
 			if inclk = '1' then
 				delay(1 to delay'high) <= delay(0 to delay'high-1);
-				delay(0) <= signed(d);
+				delay(0) <= unsigned(d);
 				sum := (others => '0');
 				for i in 0 to delay'high loop
 					sum := sum + resize(delay(i), width + sum_len);
 				end loop;
-				sum := sum + resize(signed(d), width + sum_len);
+				sum := sum + resize(unsigned(d), width + sum_len);
 				q <= std_logic_vector(sum(sum'length-1 downto sum'length-width));
 				outclk <= '1';
 			end if;
